8095f449259f345f9a8f9d8b92ba781eb62bcbdd
jonsalbert1 <jonsalbert@gmail.com>
2025-10-11 15:31:51 -0700

    Tailwind v4: add JS PostCSS config and wire global CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
-import NavBar from "../components/NavBar"; // uses baseUrl or tsconfig paths if set; otherwise use "../components/NavBar"
+import "../styles/globals.css";            // <-- required
+import NavBar from "../components/NavBar"; // ok to keep
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
